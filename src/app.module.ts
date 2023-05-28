dfa19bcb0dcf79815049665f71f9ca3523ad0c58
Dricc-new <diegoramoncc17@gmail.com>
2023-05-27 22:54:50 -0400

    render and server route created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RenderModule } from './render/render.module';
+import { ServerModule } from './server/server.module';
 
 @Module({
-  imports: [],
+  imports: [RenderModule, ServerModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/render/render.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('render')
+export class RenderController {}
