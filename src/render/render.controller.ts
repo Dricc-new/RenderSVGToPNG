5eec1f8f52f942d17ade2da0e79d0e381252d7ee
Dricc-new <diegoramoncc17@gmail.com>
2023-06-03 12:56:24 -0400

    later of  the error in git

--- src/render/render.controller.ts
+++ src/render/render.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { RenderService } from './render.service'
 import { RenderDTO } from './dto/render.dto';
 import { v4 } from 'uuid';
+import { CreatePackageDTO } from './dto/package.dto';
+import { CreateRenderDTO } from './dto/create-render.dto';
 const sharp = require("sharp");
 const fs = require('fs');
 
@@ -11,19 +13,40 @@ export class RenderController {
     
     @Post()
     Renderer(@Body() newRender: RenderDTO){
-        fs.readFile(newRender.template, 'utf-8', (err, data,r = newRender) => {
+        fs.readFile(newRender.template, 'utf-8', (err, content,r = newRender) => {
             if(!err) {
+                // Create new package
+                var packageRender = new CreatePackageDTO;
+                packageRender.cant = r.data.length;
+                packageRender.email = r.email;
+                console.log(this.renderService.createPackage(packageRender));
+                
+                //declaretion filename and temp var
                 var filename:string,temp:string;
+
+                // Iterate r.data
                 r.data.forEach(element => {
-                    temp = data;
+
+                    // Save the template in temp
+                    temp = content;
+
+                    // Generate a random filename 
                     filename = v4();
+
+                    // Change the keys to the data in the template 
                     r.keys.forEach( (key,ind)=> {
                         temp = temp.replace('{{'+key+'}}',element[ind]);
                     });
+
+                    //Save the file to local storage and generate it in the database this item
                     fs.writeFile("./storage/temp/"+filename+".svg",temp,function(err,src = filename){
                         if (!err) { 
+
+                            // Convert file svg to file png 
                             sharp("./storage/temp/"+src+".svg").png().toFile("./storage/temp/"+src+".png").then(function(info,file = src) {
-                                console.log(file);
+                                // const render = new CreateRenderDTO;
+                                // render.filename
+                                // this.renderService.createRender();
                             }).catch(function(err) {
                                 console.log("error1: "+err);
                             });
--- src/render/render.service.ts
+++ src/render/render.service.ts
@@ -11,13 +11,37 @@ export class RenderService {
     constructor(@InjectRepository(Package) private packageRepository: Repository<Package>,
                 @InjectRepository(Render) private renderRepository: Repository<Render>){}
     
-    createPackage(packageRender: CreatePackageDTO){
+    async createPackage(packageRender: CreatePackageDTO){
         const NewPackage = this.packageRepository.create(packageRender);
-        return this.packageRepository.save(NewPackage);
+        return await this.packageRepository.save(NewPackage);
     }
 
-    createRender(render: CreateRenderDTO){
+    getPackage(id: number){
+        return this.packageRepository.findOne({
+            where:{
+                id
+            }
+        });
+    }
+
+    deletePackage(id: number){
+        this.packageRepository.delete({id});
+    }
+    
+    deleteRender(id: number){
+        this.renderRepository.delete({id});
+    }
+
+    async createRender(render: CreateRenderDTO){
         const NewRender = this.renderRepository.create(render);
-        return this.renderRepository.save(NewRender);
+        const item = await this.renderRepository.save(NewRender);
+        console.log(item);
+        // una ves guardado el dato en la base de dato comprobamos si ya el packeage esta completo
+        // If completed 
+        //     Send by email
+        //         If it was sent 
+        //             Destroy files
+        //             Destroy items in the database
+        //             Destroy package in the database
     }
 }
