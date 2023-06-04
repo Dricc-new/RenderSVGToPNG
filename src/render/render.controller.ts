e4b153ad8d5c987684a7b19a4f6e32621ffa6234
Dricc-new <diegoramoncc17@gmail.com>
2023-06-04 04:00:10 -0400

    send by email good

--- src/render/render.controller.ts
+++ src/render/render.controller.ts
@@ -6,6 +6,7 @@ import { CreatePackDTO } from './dto/pack.dto';
 import { CreateRenderDTO } from './dto/create-render.dto';
 const sharp = require("sharp");
 const fs = require('fs/promises');
+const nodemailer = require('nodemailer');
 
 @Controller('render')
 export class RenderController {
@@ -21,50 +22,85 @@ export class RenderController {
             packRender.email = r.email;
             this.renderService.createPack(packRender)
             .then((dat)=>{
-                //declaretion filename and temp var
-                var filename:string,temp:string;
-                
+                var count = 0;
                 // Iterate r.data
                 r.data.forEach(element => {
                     
                     // Save the template in temp
-                    temp = content;
+                    var temp = content;
                     
                     // Generate a random filename 
-                    filename = v4();
+                    const filename = v4();
                     
                     // Change the keys to the data in the template 
                     r.keys.forEach( (key,ind)=> {
-                        temp = temp.replace('{{'+key+'}}',element[ind]);
+                        if(key[0] == '#')
+                            temp = temp.replace('{{'+key+'}}',element[ind])
+                        else temp = temp.replace('{{'+key+'}}',element[ind]);
                     });
                     
                     //Save the file to local storage and generate it in the database this item
-                    fs.writeFile("./storage/temp/"+filename+".svg",temp).
-                    then((src = filename) => {
+                    fs.writeFile("./storage/temp/"+filename+".svg",temp)
+                    .then((src = filename) => {
+
+                        // Convert SVG file to PNG file
                         return sharp("./storage/temp/"+src+".svg").png().toFile("./storage/temp/"+src+".png")
-                    })
-                    .then((info,src = filename, packId = dat.id) => {
+                    }).then((info,src = filename) => {
+                        
+                        // Delete SVG file
+                        return fs.unlink("storage/temp/"+src+".svg")
+                    }).then((src = filename, packId = dat.id) => {
+                        
                         // Load data to render
                         const render = new CreateRenderDTO;
                         render.filename = src;
-                        render.packId = packId;
-
+                        render.packId = packId;  
                         return this.renderService.createRender(render);
-                    })
-                    .then((data,packId = dat.id)=>{
-                        return this.renderService.getPack(packId)                        
-                    }).then((data) => {
-                        // If completed 
-                        if(data.renders.length == data.cant){
-                            // Send by email
+                    }).then((data: any,pack = dat)=>{
 
-                            //         If it was sent 
-                            //             Destroy files
-                            //             Destroy items in the database
-                            //             Destroy pack in the database
-                        }
+                        if(!(++count == pack.cant)) throw new Error('next')
+                        return this.renderService.getPack(pack.id)                        
+                    }).then((data: any) => {
 
+                        //Generate Attachments
+                        var attachments = [];
+                        data.renders.forEach(element => {
+                            attachments.push({path: "./storage/temp/"+element.filename+".png"})
+                        });
 
+                        // Send by email
+                        const transporter = nodemailer.createTransport({
+                            host: "sandbox.smtp.mailtrap.io",
+                            port: 2525,
+                            auth: {
+                              user: "5bad6ebe8b94cd",
+                              pass: "cf5036229da671"
+                            }
+                        });
+                        
+                        return transporter.sendMail({
+                            from: '"Fred Foo" <foo@example.com>', // sender address
+                            to: "bar@example.com, baz@example.com", // list of receivers
+                            subject: "Hello ✔", // Subject line
+                            text: "Hello world?", // plain text body
+                            html: "<b>Hello world?</b>", // html body
+                            attachments: attachments
+                        });
+                    
+                    }).then((data: any,id = dat.id)=>{
+                        return this.renderService.getPack(id)  
+                    }).then((data: any)=>{
+                        data.renders.forEach(element => {
+                            // Delete file
+                            fs.unlink("./storage/temp/"+element.filename+".png")
+                            
+                            // Delete render item
+                            this.renderService.deleteRender(element.id)
+                        });
+                        
+                        // Delete pack in the database
+                        this.renderService.deletePack(data.id)
+                        
                     }).catch((err) => {
                         console.log("error: "+err);
                     });
